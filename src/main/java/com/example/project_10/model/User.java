05ac10b9c0a8fda8bef5c6b5474a614780162220
richa <richard.rosko@kosickaakademia.sk>
2026-03-10 14:46:32 +0100

    Added class User/TestController

---
+++ src/main/java/com/example/project_10/model/User.java
@@ -0,0 +1,42 @@
+package com.example.project_10.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //Primarny kluc
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Integer age;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+}
