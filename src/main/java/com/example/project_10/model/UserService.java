540d7adba39844f36ba762b05838f925f67b9041
MARIO KUBAT <mario.kubat@kosickaakademia.sk>
2026-03-16 12:09:53 +0100

    Added password hashing using BCrypt

--- src/main/java/com/example/project_10/model/UserService.java
+++ src/main/java/com/example/project_10/model/UserService.java
@@ -3,6 +3,7 @@ package com.example.project_10.model;
 import com.example.project_10.authentication.Register;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Service
@@ -11,18 +12,27 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public void register(Register reg) {
+
         if(userRepository.findByEmail(reg.getEmail()).isPresent()) {
             throw new RuntimeException("Email already exists");
         }
+
         User user = new User();
+
         user.setEmail(reg.getEmail());
-        user.setPassword(reg.getPassword());
+
+        String hashedPassword = encoder.encode(reg.getPassword());
+        user.setPassword(hashedPassword);
+
         user.setAge(reg.getAge());
         user.setName(reg.getName());
         user.setSurname(reg.getSurname());
         user.setDateOfBirth(reg.getDate_of_birth());
         user.setNickname(reg.getNickname());
+
         userRepository.save(user);
     }
 }
