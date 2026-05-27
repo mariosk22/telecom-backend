a837ad255d37fc992e21242eb340fd8f05887232
Sebovic <sschwartz050@gmail.com>
2026-05-27 09:13:35 +0200

    added essentials files for further development of like endpoint + functional like entity and repository layer

---
+++ backend/src/main/java/com/example/project_10/repository/LikeRepository.java
@@ -0,0 +1,12 @@
+package com.example.project_10.repository;
+
+import com.example.project_10.entity.Like;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LikeRepository extends JpaRepositoryImplementation<Like,Long> {
+    boolean existsByPostIdAndUserId(Long postId, Long userId);
+    long countByPostId(Long postId);
+    void deleteByPostIdAndUserId(Long postId, Long userId);
+}
