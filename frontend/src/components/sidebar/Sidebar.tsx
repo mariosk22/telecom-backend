ec531d5d2a6ba1c1518aa6f9a353a2344122227c
Peter Šupinský <peter.supinsky@kosickaakademia.sk>
2026-05-28 13:29:30 +0200

    Error fix

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,18 +1,3 @@
-// src/components/sidebar/Sidebar.tsx
-import { useState, useEffect } from "react";
-
-type SidebarItem = {
-  name: string;
-  avatar?: string; // URL obrázka (teraz z /images/, neskôr z backendu)
-  icon?: string; // pre "Others" kategóriu
-};
-
-type Category = {
-  title: string;
-  icon: string;
-  items: SidebarItem[];
-};
-
 function Sidebar() {
   return (
     <aside className="sidebar">
