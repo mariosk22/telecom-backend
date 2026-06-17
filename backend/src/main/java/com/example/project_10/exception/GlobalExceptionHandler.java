5d35e99a8c49af93648be7359f619559ca0daccd
Sebovic <sschwartz050@gmail.com>
2026-06-17 21:29:51 +0200

    .

--- backend/src/main/java/com/example/project_10/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/project_10/exception/GlobalExceptionHandler.java
@@ -9,9 +9,8 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(ApiException.class)
     public ResponseEntity<Map<String, String>> handleExceptionApi (ApiException exception) {
         return ResponseEntity.status(exception.getStatus()).body(Map.of("message", exception.getMessage()));
     }
-
 }
