872c94ca8bbf426410d2fc283d4021cb20da59fe
Peter Šupinský <peter.supinsky@kosickaakademia.sk>
2026-06-20 00:01:42 +0200

    Bug fixes, light mode added to login / register page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,10 @@ import App from "./App.tsx";
 import "./index.css";
 import "./styles/create-post.css";
 
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light-mode");
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
